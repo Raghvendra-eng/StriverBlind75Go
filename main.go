f39aa50ff6c1c966a15abd3fd0559734062255ca
Raghvendra Pandey <raghvendrapandey@Raghvendras-MacBook-Air.local>
2025-08-19 13:32:04 +0530

    container with maximum water solution

--- main.go
+++ main.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"slices"
 )
+
 func main() {
 	// Create a new instance of the logger
 	fmt.Println("Git Repository to solve Strivers Blind 75 problems in go")
-	slice := []int{1,2,3,4,5,3,0}
+	slice := []int{1, 2, 3, 4, 5, 3, 0}
 	fmt.Println("Original Slice: ", slice)
 	slices.SortFunc(slice, func(a, b int) int {
 		return a - b
