30cc2b1950595946ef90ebc36af9f6c764642fde
Raghvendra Pandey <raghvendrapandey@Raghvendras-MacBook-Air.local>
2025-08-23 11:37:19 +0530

    missing no solution

---
+++ binary/missingNo_test.go
@@ -0,0 +1,33 @@
+package binary
+
+import "testing"
+
+func Test_missingNumber(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test1",
+			args: args{[]int{0, 1, 2, 3}},
+			want: 4,
+		},
+		{
+			name: "test1",
+			args: args{[]int{4, 1, 2, 3}},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := missingNumber(tt.args.nums); got != tt.want {
+				t.Errorf("missingNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
