06f575424e0fb8668dcd4f42780b12aeec625fba
Raghvendra Pandey <raghvendrapandey@Raghvendras-MacBook-Air.local>
2025-08-27 15:36:45 +0530

    fix test case issue

--- arrays/threeSum_test.go
+++ arrays/threeSum_test.go
@@ -10,9 +10,10 @@ func Test_threeSum(t *testing.T) {
 		nums []int
 	}
 	tests := []struct {
-		name string
-		args args
-		want [][]int
+		name  string
+		args  args
+		want  [][]int
+		want1 [][]int
 	}{
 		// TODO: Add test cases.
 		{
@@ -20,19 +21,21 @@ func Test_threeSum(t *testing.T) {
 			args: args{
 				nums: []int{-1, 0, 1, 2, -1, -4},
 			},
-			want: [][]int{{-1, -1, 2}, {-1, 0, 1}},
+			want:  [][]int{{-1, -1, 2}, {-1, 0, 1}},
+			want1: [][]int{{-1, 0, 1}, {-1, -1, 2}},
 		},
 		{
 			name: "test2",
 			args: args{
 				nums: []int{-2, 0, 1, 1, 2},
 			},
-			want: [][]int{{-2, 0, 2}, {-2, 1, 1}},
+			want:  [][]int{{-2, 0, 2}, {-2, 1, 1}},
+			want1: [][]int{{-2, 1, 1}, {-2, 0, 2}},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := threeSum(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+			if got := threeSum(tt.args.nums); !reflect.DeepEqual(got, tt.want) && !reflect.DeepEqual(got, tt.want1) {
 				t.Errorf("threeSum() = %v, want %v", got, tt.want)
 			}
 		})
