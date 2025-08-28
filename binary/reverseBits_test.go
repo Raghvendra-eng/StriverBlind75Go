cb72ee4a6864f49bbc92353fb9a1fd2935410320
Raghvendra Pandey <raghvendrapandey@Raghvendras-MacBook-Air.local>
2025-08-28 11:47:21 +0530

    reverse bit solution

---
+++ binary/reverseBits_test.go
@@ -0,0 +1,33 @@
+package binary
+
+import "testing"
+
+func Test_reverseBits(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test1",
+			args: args{43261596},
+			want: 964176192,
+		},
+		{
+			name: "test2",
+			args: args{2147483644},
+			want: 1073741822,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseBits(tt.args.n); got != tt.want {
+				t.Errorf("reverseBits() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
